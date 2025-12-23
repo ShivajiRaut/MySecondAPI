0cd07c8d3e54c8d04167fae5e5115e0648ea6261
vaibhav <vaibhav35435@gmail.com>
2025-12-23 21:26:11 +0530

    Add project files.

---
+++ MySecondAPI/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace MySecondAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Marks { get; set; }
+    }
+}
